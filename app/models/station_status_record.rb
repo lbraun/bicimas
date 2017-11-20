84c0e4cd6144db524a8f1dd7f9950465f397d09a
Lucas Braun <lucasalan@gmail.com>
2017-11-20 02:15:30 +0100

    Fix time zones again :(

--- app/models/station_status_record.rb
+++ app/models/station_status_record.rb
@@ -1,12 +1,29 @@
 class StationStatusRecord < ApplicationRecord
-  scope :from_today, -> { where('DATE(created_at) = CURRENT_DATE') }
-  scope :from_weekends, -> { where('EXTRACT(DOW FROM created_at) IN (0,6)') }
-  scope :from_week_days, -> { where('EXTRACT(DOW FROM created_at) NOT IN (0,6)') }
+  scope :from_today, -> { where("DATE(#{created_at_sql}) = DATE(#{current_timestamp_sql})") }
+  scope :from_weekends, -> { where("EXTRACT(DOW FROM #{created_at_sql}) IN (0,6)") }
+  scope :from_week_days, -> { where("EXTRACT(DOW FROM #{created_at_sql}) NOT IN (0,6)") }
 
   def self.from_hour(hour)
+    where("? = EXTRACT(HOUR FROM #{created_at_sql})", hour)
+  end
+
+  def self.postgres_time_zone
     # https://stackoverflow.com/questions/23420859/how-to-map-rails-timezone-names-to-postgresql
-    postgres_time_zone = ActiveSupport::TimeZone.find_tzinfo(Time.zone.name).identifier
-    where("? = EXTRACT(HOUR FROM (created_at AT TIME ZONE ?))", hour, postgres_time_zone)
+    ActiveSupport::TimeZone.find_tzinfo(Time.zone.name).identifier
+  end
+
+  def self.created_at_sql
+    # Time zones suck!
+    # The created_at timestamp is in UTC but is not stored with its zone in the database.
+    # First we mark it as UTC, then convert it to the app's default zone.
+    "(created_at AT TIME ZONE 'UTC' AT TIME ZONE '#{postgres_time_zone}')"
+  end
+
+  def self.current_timestamp_sql
+    # Time zones suck!
+    # The postgres CURRENT_TIMESTAMP method returns the current time in the server's time zone.
+    # We convert it to the app's default zone, in case the server is in a different zone.
+    "CURRENT_TIMESTAMP AT TIME ZONE '#{postgres_time_zone}'"
   end
 
   def created_at_s
